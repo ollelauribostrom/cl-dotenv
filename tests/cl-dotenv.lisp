5f646c6100b7a0a50d19c83426831efcb8b9c42c
Olle Lauri Boström <ollebostr@gmail.com>
2018-09-18 20:45:38 +0200

    Adding test cases

--- tests/cl-dotenv.lisp
+++ tests/cl-dotenv.lisp
@@ -7,22 +7,28 @@
 (plan 1)
 
 (subtest "cl-dotenv:read-env"
-  (subtest "READ-ENV throws error if .env file is corrupt"
+  (subtest "READ-ENV throws error if .env file contains malformed entries"
     (is-error (.env:read-env (asdf:system-relative-pathname "cl-dotenv-test"
-                                                            "./tests/.env-corrupt"))
+                                                            "./tests/.env-malformed"))
               'malformed-entry))
+
+  (subtest "READ-ENV throws error if .env file contains duplicated entries"
+    (is-error (.env:read-env (asdf:system-relative-pathname "cl-dotenv-test"
+                                                            "./tests/.env-duplicated"))
+              'duplicated-entry))
 
   (subtest "READ-ENV loads a .env file"
     (let ((env (.env:read-env (asdf:system-relative-pathname "cl-dotenv-test"
                                                              "./tests/.env-test"))))
       (is (gethash "TEST_VAR_1" env) "test1")
-      (is (gethash "TEST_VAR_2" env) "test2")))
+      (is (gethash "TEST_VAR_2" env) "test2")
+      (is (gethash "TEST_VAR_3" env) "")))
 
   (subtest "LOAD-ENV  sets the environment"
     (.env:load-env (asdf:system-relative-pathname "cl-dotenv-test"
                                                   "./tests/.env-test"))
-
     (is (uiop:getenv "TEST_VAR_1") "test1")
-    (is (uiop:getenv "TEST_VAR_2") "test2")))
+    (is (uiop:getenv "TEST_VAR_2") "test2")
+    (is (uiop:getenv "TEST_VAR_3") "")))
 
 (finalize)
